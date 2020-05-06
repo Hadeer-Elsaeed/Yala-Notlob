8416e0398d1f6b82807a0e2e09d4a6ae0f36310d
Mariam Elfeky <mariamelfeky123@gmail.com>
2020-05-06 03:23:50 +0200

    fix list friends activity

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,14 @@ class HomeController < ApplicationController
   
   def index
     @latest_orders = Order.select("meal","created_at","resturant").where("User_id=?",current_user.id).last(5).reverse
-    @friends_order = Order.select("meal","created_at","resturant").where.not("User_id=?",current_user.id).last(5).reverse
+    # @order = current_user.followers
+    # @orders = Order.includes(current_user.followers).select('User_id').distinct
+    @orders = Order.joins(:User).where("users.id" => current_user.follower_ids).select('User_id').distinct
+
+    # @friends.each do |friend|
+    #       @friends_order = Order.select("meal","created_at","resturant").where("User_id=?",friend.id)
+    # end
 
     # @friends_name = User.includes(:orders).where(id: current_user.follower_ids)
-    # @friends_order = Order.includes("User_id=?",@friends_name)
-
-    @friends = current_user.followers
   end
 end
