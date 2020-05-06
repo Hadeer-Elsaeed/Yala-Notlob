45464b5c537ab8b8e098599b8b49f16f2089e129
Hadeer-Elsaeed <hadeerelsaeed00@gmail.com>
2020-05-06 06:17:34 +0200

    order_group_problem

--- db/migrate/20200505221141_create_order_groups.rb
+++ db/migrate/20200505221141_create_order_groups.rb
@@ -1,5 +1,6 @@
 class CreateOrderGroups < ActiveRecord::Migration[6.0]
   def change
+    # drop_table :order_groups
     create_table :order_groups do |t|
       t.integer :group_id
       t.integer :order_id
