f20b225112f7100c5dcca0981d561a4c846668ec
esraa708 <princessoftears123@gmail.com>
2020-05-07 04:41:55 +0200

    handling js

--- README.md
+++ README.md
@@ -1,41 +1,4 @@
-# README
-
-This README would normally document whatever steps are necessary to get the
-application up and running.
-
-Things you may want to cover:
-
-- Ruby version
-
-- System dependencies
-
-- Configuration
-
-- Database creation
-
-- Database initialization
-
-- How to run the test suite
-
-- Services (job queues, cache servers, search engines, etc.)
-
-- Deployment instructions
-
-- ...
-
-1-Authentication
-
-<pre>
-- The user can Register using facebook or google account
-- The User can Upload Profile Image
-- The user can Login using facebook or google account
-- Users can't redirect to any page without login
-- User can see his name and profile image in navbar
-- User can edit his profile
-</pre>
-
-### -Project Images
-![](app/assets/images/login.jpg)
-![](app/assets/images/navbar.jpg)
-![](app/assets/images/register.jpg)
-
+esraa work:-
+-Database design and modeling
+-friends
+-orders
--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -7,6 +7,29 @@ function chooseType(t) {
     $("#friendSelect").show();
   }
 }
-// function renderFriend(event) {
-//   console.log(event.target.value);
-// }
+function renderFriend(event) {
+  let id=event.target.value
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:3000/user/"+id,
+    success: function(result){
+      var newDiv = $("<div/>")   // creates a div element
+        .attr("id", result.id)  // adds the id
+        .addClass("mycard2 ml-1 my-1")   // add a class
+        .html(`<div><img src=${result.avatar}/> </div> <p>${result.name}</p>`);
+      console.log(result)
+      // var details=$('div').addClass("details");
+      // var img = $('img').attr('src', result.avatar)
+      // var name=$('p').text(result.name)
+      // // details.append(img)
+      // details.append(name)
+      $("#myblock").append(newDiv)
+
+    },
+    error:function(error){
+      // alert(error)
+    }
+
+  })
+ 
+}
